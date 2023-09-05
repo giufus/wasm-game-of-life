97e54e68483d20973562685ee1b8a0fc2f9695e3
giuseppe.fusacchia <giuseppe.fusacchia@assistdigital.com>
2023-09-05 15:47:31 +0200

    initial

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(format!("Hello, this is {name} wasm-game-of-life!").as_str());
+}
+
+
